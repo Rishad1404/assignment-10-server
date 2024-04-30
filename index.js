a8c60acfaf9a3b0216a18066658951af53d1243a
Rishad1404 <rishadislam2019@gmail.com>
2024-04-30 23:16:18 +0600

    final push

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app=express();
 const port =process.env.PORT || 5000;
 
 // MiddleWare
-app.use(cors())
+app.use(cors({
+  origin:["http://localhost:5173","https://lumina-art-and-craft.web.app"]
+}))
 app.use(express.json())
 
 
@@ -24,6 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const craftCollection=client.db('craftStore').collection('crafts')
+    const subcategoryCollection=client.db('craftStore').collection('subcategory');
 
 
     app.get('/crafts',async(req,res)=>{
@@ -35,6 +38,23 @@ async function run() {
       const id=req.params.id;
       const query={_id:new ObjectId(id)}
       const result=await craftCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.get('/subcategory',async(req,res)=>{
+      const cursor=subcategoryCollection.find()
+      const result=await cursor.toArray()
+      res.send(result)
+    })
+
+    // app.get('/subcategory/:id',async(req,res)=>{
+    //   const result=await subcategoryCollection.findOne({_id:new ObjectId(req.params.id)})
+    //   res.send(result)
+    // })
+
+    app.post('/subcategory',async(req,res)=>{
+      const result=await subcategoryCollection.insertOne(req.body);
+      console.log(result)
       res.send(result)
     })
 
@@ -89,7 +109,7 @@ async function run() {
   
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
